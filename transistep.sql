3fe9d0ac1b85b4cab7b942149dcf461f414ade45
Skulrag <58217414+Skulrag@users.noreply.github.com>
2019-11-28 13:42:28 +0100

    Update transistep.sql

--- transistep.sql
+++ transistep.sql
@@ -1,18 +1,36 @@
-INSERT INTO `addon_account` (name, label, shared) VALUES
-	('society_transistep', 'Transistep', 1)
+INSERT INTO `addon_account` (name, label, shared)
+VALUES ('society_transistep', 'Transistep', 1)
 ;
 
-INSERT INTO `addon_inventory` (name, label, shared) VALUES
-	('society_transistep', 'Transistep', 1)
+INSERT INTO `addon_inventory` (name, label, shared)
+VALUES	('society_transistep', 'Transistep', 1) 
 	('society_transistep_depot', 'Transistep Dépôt', 1)
 ;
 
-INSERT INTO `jobs` (name, label) VALUES
-	('transistep', 'Transistep')
+INSERT INTO `jobs` (name, label)
+VALUES ('transistep', 'Transistep')
 ;
 
-INSERT INTO `job_grades` (job_name, grade, name, label, salary, skin_male, skin_female) VALUES
-	('transistep',0,'recrue','Recrue',0,'{}','{}'),
-	('transistep',1,'transporter','Transporteur',0,'{}','{}'),
-	('transistep',2,'boss','Boss',0,'{}','{}')
+INSERT INTO `job_grades` (job_name, grade, name, label, salary, skin_male, skin_female)
+VALUES ('transistep', 0, 'recrue', 'Recrue', 0, '{}', '{}'),
+       ('transistep', 1, 'transporter', 'Transporteur', 0, '{}', '{}'),
+       ('transistep', 2, 'boss', 'Boss', 0, '{}', '{}')
 ;
+
+CREATE TABLE `convoy_list`
+(
+    id         int(11) NOT NULL
+);
+
+INSERT INTO `convoy_list` (id)
+VALUES (1),(2),(3),(4),(5),(6),(7),(8); -- add as many as you want
+
+CREATE TABLE `convoy_registered_list`
+(
+    `convoy_id`         int(11) NOT NULL,
+    `identifier`        varchar(60) NOT NULL,
+    `is_trailer_stored` boolean,
+    `is_paid`           boolean,
+
+    PRIMARY KEY (`convoy_id`)
+);
